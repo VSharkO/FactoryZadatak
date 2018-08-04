f348d52fde930eed929f9671e488b57387039b20
VSharkO <valentin.saric@hotmail.com>
2018-08-04 19:06:02 +0200

    added timber

--- app/src/main/java/com/example/vsharko/factoryzadatak/App.java
+++ app/src/main/java/com/example/vsharko/factoryzadatak/App.java
@@ -2,13 +2,23 @@ package com.example.vsharko.factoryzadatak;
 
 import android.app.Application;
 import android.support.annotation.NonNull;
+
+import com.bumptech.glide.RequestBuilder;
 import com.example.vsharko.factoryzadatak.networking.helpers.NetworkingHelper;
 import com.example.vsharko.factoryzadatak.networking.helpers.NetworkingHelperImpl;
 import com.example.vsharko.factoryzadatak.networking.NewsAPIService;
 import com.example.vsharko.factoryzadatak.utils.Constants;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import timber.log.Timber;
 
 public class App extends Application {
     private Retrofit retrofit;
@@ -22,7 +32,7 @@ public class App extends Application {
         retrofit = provideRestClient();
         NewsAPIService service = createNewsAPIService(retrofit);
         this.networkingHelper = new NetworkingHelperImpl(service);
-
+        Timber.plant(new Timber.DebugTree());
     }
 
     private NewsAPIService createNewsAPIService(Retrofit retrofit) {
@@ -32,8 +42,25 @@ public class App extends Application {
     @NonNull
     private Retrofit provideRestClient() {
 
+        OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
+
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override
+            public void log(String message) {
+                Timber.d(message);
+            }
+        });
+
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+         if(BuildConfig.DEBUG) {
+             okHttpClientBuilder.addInterceptor(logging);
+         }
+
+
         return new Retrofit.Builder()
                 .baseUrl(Constants.NEWS_API_BASE_URL)
+                .client(okHttpClientBuilder.build())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
--- app/src/main/java/com/example/vsharko/factoryzadatak/networking/helpers/NetworkingHelperImpl.java
+++ app/src/main/java/com/example/vsharko/factoryzadatak/networking/helpers/NetworkingHelperImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import timber.log.Timber;
 
 public class NetworkingHelperImpl implements NetworkingHelper {
 
@@ -32,7 +33,7 @@ public class NetworkingHelperImpl implements NetworkingHelper {
                     ArticlesList data = response.body();
                     if(data!=null)
                         listener.onSuccess(data.getArticles());
-
+                    Timber.i("ušo");
                 }
 
             }
